5c45ec0769d63444d594a35b00478ce4980724ce
NSRana <45749989+NSRana@users.noreply.github.com>
2018-12-11 09:26:31 +0530

    Create Assg3.py

---
+++ Assg3.py
@@ -0,0 +1,9 @@
+#10/12/2018
+#Assignment 3: Identifying the type of variable
+
+bill_id=1001
+customer_id=101
+bill_amount=199.99
+print("bill_id:",type(bill_id))
+print("customer_id:",type(customer_id))
+print("bill_amount:",type(bill_amount))
